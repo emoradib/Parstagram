690b5fcf2f0c70e93292229cf2835d002d6924f8
eamon moradi <emoradib@ucsd.edu>
2020-03-03 15:18:19 -0800

    Trying to get the comments to show

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,30 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Eamon J Moradi-bidhendi on 3/3/20.
+//  Copyright © 2020 Eamon J Moradi-bidhendi. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
